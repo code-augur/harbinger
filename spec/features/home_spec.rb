93e4c096d19b3a0e94607ebe5ef4cf25775e3bfe
tnesbitt <trever.nesbitt@gmail.com>
2014-06-21 23:13:18 -0400

    updated home_spec to try out testing framework, still working out bugs

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe "Home Page" do 
+
+  it "should have the title 'Harbinger'", :pending => 'poltergeist is not rendering page' do
+    page.visit "/"
+    expect(page).to have_content('Harbinger')
+  end
 end
 
